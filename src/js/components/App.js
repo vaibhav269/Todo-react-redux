9c239947a088fd461558c182555f7f8efc8961f8
Vaibhav <vaibhav.verma2697@gmail.com>
2018-08-21 01:50:40 +0530

    customizing styles for mobile

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
 
-const App = ()=>(
-    <div>
-        <div className="container-fluid">
-            <div className="row mt-5 justify-content-center">
-                <h1 className="display-2 text-center">WELCOME TO THE TODO APPLICATION</h1>    
-            </div>
-            <div className="row mt-5 justify-content-center">                
-                <div className="col-sm-12 col-lg-7">
-                    <div className="row">                        
-                        <Form/>                        
-                    </div>
 
-                    <div className="row mt-5">
-                        <List />
-                    </div>                
+const breakpoints = {
+    desktop: 1040,
+    tablet: 840,
+    mobile: 540
+  };
+
+
+class App extends Component{
+    render(){
+        function responsiveHeading(){            
+                if (window.innerWidth > breakpoints.tablet) {
+                    return (
+                            <h1 className="display-2 mt-5 text-center">WELCOME TO TODO APPLICATION</h1> 
+                        )   
+                  }            
+            else {
+                return (
+                    <h1 style ={ {backgroundColor : '#ee6352'}} className="display-4 text-center text-white w-100"><strong>TODOS</strong></h1> 
+                )
+            }
+        }
+        return(
+            <div>
+                <div className="container-fluid">
+                    <div className="row justify-content-center">        
+                        { responsiveHeading() }   
+                    </div>
+                <div className="row mt-5 justify-content-center">                
+                    <div className="col-sm-12 col-lg-7">
+                        <div className="row">                        
+                            <Form/>                        
+                        </div>
+    
+                        <div className="row mt-5">
+                            <List />
+                        </div>                
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        </div>        
+        )
+    }
+}
+
 
 export default App;
